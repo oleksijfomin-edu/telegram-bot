89c422bdc352aafe868518d17be110ecebdb8043
oleksijfomin <oleksij.fomin@gmail.com>
2024-02-19 13:58:41 +0200

    feat: add initial project changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+// Функція-обробник запитів
+const requestHandler = (request, response) => {
+    console.log('Запит отримано.');
+    response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'}); // Встановлюємо правильне кодування
+    response.end('Привіт з веб-сервера Node.js!');
+};
+
+// Створення сервера
+const server = http.createServer(requestHandler);
+
+// Прослуховування на порту 3000
+server.listen(3000, (err) => {
+    if (err) {
+        return console.log('Щось пішло не так:', err);
+    }
+
+    console.log('Сервер працює на http://localhost:3000');
+});
