dfa215455c3dfb990f985b85c068f1a01bc6494d
oleksijfomin <oleksij.fomin@gmail.com>
2024-02-26 14:05:42 +0200

    feat: add telegram bot implementation
    - change start command
    - provide new env variables

---
+++ server-bot.js
@@ -0,0 +1,19 @@
+const { Telegraf } = require('telegraf')
+const { message } = require('telegraf/filters')
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch({
+  webhook: {
+    domain: process.env.WEBHOOK_DOMAIN,
+    port: process.env.PORT,
+  },
+})
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
