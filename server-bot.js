f1eafc22c192c0a2b34d50f6718ca083dd870723
maksympalchei <148455517+maksympalchei@users.noreply.github.com>
2024-03-24 16:01:35 +0200

    feat: Add response for greeting messages
    Add functionality to respond with "Привіт, друже!" when the user sends "Привіт".

--- server-bot.js
+++ server-bot.js
@@ -27,7 +27,7 @@ bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
+bot.hears('Привіт', (ctx) => ctx.reply('Привіт, друже!'))
 
 // Обробник вхідних повідомлень бота
 bot.hears('gpt', async (ctx) => {
